5d8fc667d7f2d6fc32a144f200a48d89ac2c3337
BeatEngine <32369318+BeatEngine@users.noreply.github.com>
2020-08-27 14:08:06 +0200

    Version 2.0 Compile (static)
     - Button
     - InputBox
     - RadioBox and RadioGroup
     - CheckBox
     - TabbedContainer
     - List
     - ScrollBox
     - MenuList and ContextMenu
     - ProgressBar (Rounded)

--- compile.sh
+++ compile.sh
@@ -1,4 +1,4 @@
 #/bin/bash
-g++ SDLlightGUI.cpp -W -lSDL2 -lSDL2_ttf -o SDLlightGUI
-
+g++ SDLlightGUI.cpp -static-libgcc -static-libstdc++ -lSDL2 -lSDL2main -lSDL2_ttf -lSDL2_image -o SDLLightGUI
+g++ SDLlightGUI.cpp -static-libgcc -static-libstdc++ -lSDL2 lib/libSDL2main.a -lSDL2_ttf lib/libSDL2_image.a -o SDLLightGUIstatic
 
